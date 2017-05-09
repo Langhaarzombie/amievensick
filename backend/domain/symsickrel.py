66b6b5600d8cc8538cd76356780d1ac8da9c0246
Maximilian Meyer-Mölleringhof <mey16278@spengergasse.at>
2017-05-09 13:26:33 +0200

    Cleaned up domain - should be finished now

--- backend/domain/symsickrel.py
+++
@@ -1,9 +0,0 @@
-import json
-
-class SymSickRel: #a class to represent the realtion between a sickness and a symptom
-	def __init__(self, frequency, location):
-		self.frequency = frequency
-		self.location = location
-
-	def toJSON(self):
-		return json.dumps({"symsickrel": {"frequency": self.frequency, "location": self.location}})
