b94fa319c35c5bfac45b529c7461a11176d6d309
Maximilian Meyer-Mölleringhof <mey16278@spengergasse.at>
2017-05-11 17:30:38 +0200

    Implemented Singleton for Connector, improvements in tests

--- backend/persistance/neo4chain/dbmanager.py
+++ backend/persistance/neo4chain/dbmanager.py
@@ -23,6 +23,38 @@ class Connection: # manages the connection to the database
 	def disconnect(self):
 		self.getSession().close()
 
+class Connector:
+	class Connection:
+		def getDriver(self):
+			if(not hasattr(self, 'driver')):
+				#default: bolt://localhost:7687, neo4j, neo4j
+				self.driver = GraphDatabase.driver(self.url, auth=basic_auth(self.username, self.password))
+			return self.driver
+
+		def getSession(self):
+			if(not hasattr(self, 'session')):
+				self.session = self.getDriver().session()
+			return self.session
+
+		def disconnect(self):
+			self.getSession().close()
+
+		def __init__(self, url, username, password):
+			self.url = url
+			self.username = username
+			self.password = password
+			self.session = getSession()
+
+	connection = None
+
+	def __init__(self, url = None, username = None, password = None):
+		if(not Connector.connection):
+			Connector.connection = Connection(url, username, password)
+
+	def setAttributes(self, url, username, password):
+		Connector.connection.disconnect()
+		Connector.connection = Connection(url, username, password)
+
 class GarbageCollector: # responsible for deleting relations and nodes that were created while testing
 	def __init__(self, session):
 		self.session = session
---
+++ backend/persistance/symptomrepo.py
@@ -0,0 +1,16 @@
+from neo4chain import commands, dbmanager
+
+def createSymptom(symptom):
+	cmd = commands.Command().addNode("symptom", "s1").setProperty("name", symptom.name, "s1")
+	
+	# Debug
+	print(Executing: )
+	cmd.printCommand()
+	
+	result = cmd.execute(dbmanager.Connection().getSession())
+
+	# Debug
+	print(result)
+
+def saveSymptom(symptom):
+	pass
