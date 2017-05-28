d84934d1ababf03ef654f2e506a5e13202b7da82
Maximilian Meyer-Mölleringhof <mey16278@spengergasse.at>
2017-05-28 12:21:54 +0200

    Added possibility to create syms and sicks

--- api.py
+++ api.py
@@ -1,11 +1,13 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask import Response, request
 
 from backend import service
 
 app = Flask(__name__)
 api = Api(app)
+
+## Default stuff ##
 
 @app.route('/', methods=['GET'])
 def hello():
@@ -25,6 +27,12 @@ def getSymptomByName(sym):
 	resp = Response(out, status=200, mimetype='application/json')
 	return resp
 
+@app.route('/symptom', methods=['POST'])
+def createSymptom():
+	income = request.get_json()
+	out = service.createSymptom(income)
+	return "Success! Info: " + str(out)
+
 ## Sickness Action ##
 
 @app.route('/sickness', methods=['GET'])
@@ -38,6 +46,12 @@ def getSicknessByName(sick):
 	out = service.getSicknessByName(sick)
 	resp = Response(out, status=200, mimetype='application/json')
 	return resp
+
+@app.route('/symptom', methods=['POST'])
+def createSickness():
+	income = request.get_json()
+	out = service.createSickness(income)
+	return "Success! Info: " + str(out)
 
 ## Combined Actions ##
 
--- backend/service.py
+++ backend/service.py
@@ -2,21 +2,40 @@ from domain import symptom, sickness, indicates
 from persistance import sicknessrepo, symptomrepo
 import convert
 
+## Symptom Actions ##
+
 def getAllSymptoms():
 	result = symptomrepo.getAllSymptoms()
 	return convert.symptomBoltToJSON(result)
-
-def getAllSicknesses():
-	result = sicknessrepo.getAllSicknesses()
-	return convert.sicknessBoltToJSON(result)
 
 def getSymptomByName(name):
 	result = symptomrepo.getSymptomByName(str(name))
 	return convert.symptomBoltToJSON(result)
 
+def createSymptom(json):
+	name = json.get("symptom").get("name")
+	result = symptomrepo.createSymptom(symptom.Symptom(0, str(name))) # Id does not matter at this point
+	return result
+
+## Sickness Actions ##
+
+def getAllSicknesses():
+	result = sicknessrepo.getAllSicknesses()
+	return convert.sicknessBoltToJSON(result)
+
 def getSicknessByName(name):
 	result = sicknessrepo.getSicknessByName(str(name))
 	return convert.sicknessBoltToJSON(result)
+
+def createSickness(json):
+	name 					= json.get("sickness").get("name")
+	requires_medication 	= json.get("sickness").get("requires_medication")
+	prevalence 				= json.get("sickness").get("prevalence")
+	location 				= json.get("sickness").get("location")
+	result = sicknessrepo.createSickness(sickness.Sickness(0, str(name), str(requires_medication), str(prevalence), str(location))) # Id does not matter at this point
+	return result
+
+## Combined Actions ##
 
 def getSicknessBySymptoms(json):
 
